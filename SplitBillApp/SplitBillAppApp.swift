9e4c3c5bb560375b8c9bbd5b95678dfd4deec134
Yuva <user@example.com>
2023-07-16 14:38:11 +0530

    Initial Commit

---
+++ SplitBillApp/SplitBillAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillAppApp.swift
+//  SplitBillApp
+//
+//  Created by Yuvakumar M on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
